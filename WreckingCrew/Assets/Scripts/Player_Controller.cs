8aa310cde9893069a6abaa4517fbb005ca565093
Hanu Kancharla <kancharla.hanu@gmail.com>
2022-05-11 18:58:54 -0700

    Improved crane speed and crane rotation speed

--- WreckingCrew/Assets/Scripts/Player_Controller.cs
+++ WreckingCrew/Assets/Scripts/Player_Controller.cs
@@ -7,6 +7,12 @@ public class Player_Controller : MonoBehaviour
 {
     private Transform crane_arm;
 
+    #region Crane variables
+    [Header("Crane Variables")]
+    [SerializeField] float crane_Speed = 4f;
+    [SerializeField] float crane_Rotation_Speed = 2.5f;
+    #endregion
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,18 +24,18 @@ public class Player_Controller : MonoBehaviour
     {
         TiltFive.Input.TryGetStickTilt(out Vector2 stick_tilt);
         if(Mathf.Abs(stick_tilt.x) > 0.5f) {
-            transform.Rotate(0, stick_tilt.x*60*Time.deltaTime, 0);
+            transform.Rotate(0, stick_tilt.x * 60 * Time.deltaTime * crane_Rotation_Speed, 0);
         } else if(UnityEngine.Input.GetKey("a")) {
-            transform.Rotate(0, -1*60*Time.deltaTime, 0);
+            transform.Rotate(0, -1 * 60 * Time.deltaTime * crane_Rotation_Speed, 0);
         } else if(UnityEngine.Input.GetKey("d")) {
-            transform.Rotate(0, 60*Time.deltaTime, 0);
+            transform.Rotate(0, 60 * Time.deltaTime * crane_Rotation_Speed, 0);
         }
         if(Mathf.Abs(stick_tilt.y) > 0.5f) {
-            transform.position += transform.forward*stick_tilt.y*Time.deltaTime;
+            transform.position += transform.forward * stick_tilt.y * Time.deltaTime * crane_Speed;
         } else if(UnityEngine.Input.GetKey("w")) {
-            transform.position += transform.forward*Time.deltaTime;
+            transform.position += transform.forward * Time.deltaTime * crane_Speed;
         } else if(UnityEngine.Input.GetKey("s")) {
-            transform.position += -1*transform.forward*Time.deltaTime;
+            transform.position += -1*transform.forward * Time.deltaTime * crane_Speed;
         }
 
         crane_arm.localRotation = Quaternion.Lerp(crane_arm.localRotation, TiltFive.Wand.GetRotation(), 3*Time.deltaTime);
