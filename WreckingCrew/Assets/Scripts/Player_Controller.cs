7691437e65e4bed596c090e0d89969059c9b1ed3
Asj1098 <63750078+Asj1098@users.noreply.github.com>
2022-05-25 14:18:15 -0700

    Stick Movement added

--- WreckingCrew/Assets/Scripts/Player_Controller.cs
+++ WreckingCrew/Assets/Scripts/Player_Controller.cs
@@ -14,7 +14,8 @@ public class Player_Controller : MonoBehaviour
     private float stun_apex = 0;
     private Vector3 stun_orientation_position;
 
-    [Header("Rotation Settins")]
+    [Header("Movement Settings")]
+    public bool enableTankMode = false;
     public bool rotateWorld = true;
     public GameObject rotatableWorld;
     public float rotationSpeed = 60.0f;
@@ -47,49 +48,83 @@ public class Player_Controller : MonoBehaviour
     {
         UI.transform.position = new Vector3(transform.position.x - 16.75f, transform.position.y - 1.15f, transform.position.z - 5.75f);
         TiltFive.Input.TryGetStickTilt(out Vector2 stick_tilt);
-        if (!stunned) { 
-            //Rotate player
-            if(Mathf.Abs(stick_tilt.x) > 0.5f) {
-                if (!rotateWorld)
-                    transform.Rotate(0, stick_tilt.x * 60 * Time.deltaTime * crane_Rotation_Speed, 0);
-                else
-                    rotatableWorld.transform.RotateAround(transform.position, Vector3.up, stick_tilt.x * rotationSpeed * Time.deltaTime * crane_Rotation_Speed);
-            } else if(UnityEngine.Input.GetKey("a")) {
-                if (!rotateWorld)
-                    transform.Rotate(0, -1 * 60 * Time.deltaTime * crane_Rotation_Speed, 0);
-                else
-                    rotatableWorld.transform.RotateAround(transform.position, Vector3.up, -1 * rotationSpeed * Time.deltaTime * crane_Rotation_Speed);
-            } else if(UnityEngine.Input.GetKey("d")) {
-                if (!rotateWorld)
-                    transform.Rotate(0, 60 * Time.deltaTime * crane_Rotation_Speed, 0);
-                else
-                    rotatableWorld.transform.RotateAround(transform.position, Vector3.up, 1 * rotationSpeed * Time.deltaTime * crane_Rotation_Speed);
-            }
+        if (!stunned) {
 
-            //Move player forward/backward
-            if(Mathf.Abs(stick_tilt.y) > 0.5f) {
-                transform.position += transform.forward * stick_tilt.y * Time.deltaTime * crane_Speed;
-                //UI.transform.position += transform.forward * stick_tilt.y * Time.deltaTime * crane_Speed;
-            } else if(UnityEngine.Input.GetKey("w")) {
-                transform.position += transform.forward * Time.deltaTime * crane_Speed;
-                //UI.transform.position += transform.forward * Time.deltaTime * crane_Speed;
-            } else if(UnityEngine.Input.GetKey("s")) {
-                transform.position += -1*transform.forward * Time.deltaTime * crane_Speed;
-                //UI.transform.position += -1 * transform.forward * Time.deltaTime * crane_Speed;
-            }
+            if (enableTankMode)
+            {
+                //Rotate player
+                if (Mathf.Abs(stick_tilt.x) > 0.5f)
+                {
+                    if (!rotateWorld)
+                        transform.Rotate(0, stick_tilt.x * 60 * Time.deltaTime * crane_Rotation_Speed, 0);
+                    else
+                        rotatableWorld.transform.RotateAround(transform.position, Vector3.up, stick_tilt.x * rotationSpeed * Time.deltaTime * crane_Rotation_Speed);
+                }
+                else if (UnityEngine.Input.GetKey("a"))
+                {
+                    if (!rotateWorld)
+                        transform.Rotate(0, -1 * 60 * Time.deltaTime * crane_Rotation_Speed, 0);
+                    else
+                        rotatableWorld.transform.RotateAround(transform.position, Vector3.up, -1 * rotationSpeed * Time.deltaTime * crane_Rotation_Speed);
+                }
+                else if (UnityEngine.Input.GetKey("d"))
+                {
+                    if (!rotateWorld)
+                        transform.Rotate(0, 60 * Time.deltaTime * crane_Rotation_Speed, 0);
+                    else
+                        rotatableWorld.transform.RotateAround(transform.position, Vector3.up, 1 * rotationSpeed * Time.deltaTime * crane_Rotation_Speed);
+                }
 
+                //Move player forward/backward
+                if (Mathf.Abs(stick_tilt.y) > 0.5f)
+                {
+                    transform.position += transform.forward * stick_tilt.y * Time.deltaTime * crane_Speed;
+                    //UI.transform.position += transform.forward * stick_tilt.y * Time.deltaTime * crane_Speed;
+                }
+                else if (UnityEngine.Input.GetKey("w"))
+                {
+                    transform.position += transform.forward * Time.deltaTime * crane_Speed;
+                    //UI.transform.position += transform.forward * Time.deltaTime * crane_Speed;
+                }
+                else if (UnityEngine.Input.GetKey("s"))
+                {
+                    transform.position += -1 * transform.forward * Time.deltaTime * crane_Speed;
+                    //UI.transform.position += -1 * transform.forward * Time.deltaTime * crane_Speed;
+                }
+                if (!reversing && (stick_tilt.y < -0.35f || UnityEngine.Input.GetKey("s")))
+                {
+                    reversing = true;
+                    audio_reverse.Play();
+                }
+                if (reversing && !(stick_tilt.y < -0.35f || UnityEngine.Input.GetKey("s")))
+                {
+                    reversing = false;
+                    audio_reverse.Stop();
+                }
+            }
+            else
+            {
+                Vector3 moveVector = Vector3.zero;
+
+                moveVector.x = stick_tilt.x;
+                moveVector.z = stick_tilt.y;
+                if (Mathf.Abs(UnityEngine.Input.GetAxis("Horizontal")) > 0)
+                    moveVector.x = UnityEngine.Input.GetAxis("Horizontal");
+                if (Mathf.Abs(UnityEngine.Input.GetAxis("Vertical")) > 0)
+                    moveVector.z = UnityEngine.Input.GetAxis("Vertical");
+
+                moveVector.y = 0;
+
+                if (moveVector.magnitude > 0)
+                {
+                    //transform.Translate(transform.forward * moveVector.magnitude * crane_Speed * Time.deltaTime);
+                    transform.position += transform.forward * moveVector.magnitude * Time.deltaTime * crane_Speed;
+                    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(moveVector), 8 * Time.deltaTime);
+                }
+            }
             //Check against world boundaries
             transform.position = new Vector3(Mathf.Max(-21, Mathf.Min(22.5f, transform.position.x)), transform.position.y, Mathf.Max(-22.5f, Mathf.Min(21, transform.position.z)));
 
-            //Play/Stop reverse audio
-            if(!reversing && (stick_tilt.y < -0.35f || UnityEngine.Input.GetKey("s"))) {
-                reversing = true;
-                audio_reverse.Play();
-            }
-            if(reversing && !(stick_tilt.y < -0.35f || UnityEngine.Input.GetKey("s"))) {
-                reversing = false;
-                audio_reverse.Stop();
-            }
 
             //Trigger for lengthen chain
             if (TiltFive.Input.GetTrigger() > 0.5f)
